027b2d8e991846b42f2281fcb5ae3affc7b2d0ab
dragonq1 <quinten26aug@hotmail.com>
2019-02-11 11:15:17 +0100

    Login pagina
    Basis login pagina

---
+++ index.php
@@ -0,0 +1,71 @@
+<html>
+<head>
+
+<?php include_once("php/header.php") ?>
+
+</head>
+<body>
+
+    <div class="body_loginpage">
+        <div class="body__loginbox">
+          <p class="text__title--center">Inloggen</p>
+            <form class="" action="index.php" method="post">
+                <div class="form-group body__loginbox--groups">
+                  <!-- <label for="email">E-mail</label> -->
+                  <input type="email" name="email" class="input__login" placeholder="e-mail" required>
+                </div>
+                <div class="form-group body__loginbox--groups">
+                  <!-- <label for="password">Wachtwoord</label> -->
+                  <input type="password" name="password" class="input__login" placeholder="wachtwoord" required>
+                </div>
+                <input type="submit" name="btnLogin" value="Inloggen" class="btn btn__login">
+            </form>
+            <button value="Registreren" class="btn btn__register"><a href="register.php"></a>Registeren</button>
+
+
+            <?php
+
+
+
+              if((isset($_POST["password"])) && (isset($_POST["email"])) && ($_SERVER["REQUEST_METHOD"] == "POST")) {
+
+
+                  require 'php/db.php';
+                  $con = mysqli_connect($host, $user, $pass, $db);
+
+                  $email = $con->escape_string($_POST["email"]);
+
+                  if(!$con) {
+                    throw new Exception ('Could not connect: ' . mysqli_error());
+                  }else{
+
+                      $statement = mysqli_prepare($con, "SELECT Password FROM dragv_dev.users where Email = ?");
+                      mysqli_stmt_bind_param($statement, "s", $email);
+                      mysqli_stmt_execute($statement);
+                      $result = $statement->get_result();
+
+                      if(mysqli_num_rows($result) == 1) {
+                          while($row = mysqli_fetch_assoc($result)) {
+                              $checkHash = $row["Password"];
+                          }
+
+                          if(password_verify($_POST["password"], $checkHash)){
+
+                              header("Location: succes.php");
+                          }
+                      }
+                        echo '<p class="text__error">Foutieve inloggegevens</p>';
+                  }
+              }
+
+             ?>
+        </div>
+
+    </div>
+
+
+
+
+</body>
+
+</html>
---
+++ php/db.php
@@ -0,0 +1,6 @@
+<?php
+$host = '185.114.157.174:3306';
+$user = 'dragv_dev';
+$pass = 'Y9CtowbyfutORspVqfxw';
+$db = 'dragv_dev';
+?>
