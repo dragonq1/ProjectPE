433496ec81ab01fcf1460e51094abf21cbd12a29
Dragoonix2000 <reinoud.keu@outlook.com>
2019-02-28 11:49:48 +0100

    Css home pagina veranderd&Account info werkend

--- php/Account.php
+++ php/Account.php
@@ -14,16 +14,38 @@ if(($_SERVER["REQUEST_METHOD"] == "POST") && isset($_POST["homeMenu"])) {
   mysqli_stmt_execute($statement1);
   $result1 = $statement1->get_result();
 
+
+if(mysqli_num_rows($result1)>=1)
+{
+  while($row = mysqli_fetch_assoc($result1))
+   {
+    $firstname = $row['FirstName'];
+    $lastname = $row['LastName'];
+    $nickname = $row['NickName'];
+    $email = $row['Email'];
+   }
+}
+
 //account body
 
 echo("
     <div class=\"Body__account\">
-    <div class=\"Account__info\">
-    <div class=\"Account__title\">
-    <h2>Mijn account</h2>
-    </div>
-    <p class=\"Account__info__text\">
+        <div class=\"Account__info\">
+            <div class=\"Account__title\">
+            <h2>Mijn account</h2>
+            </div>
 
+           <div class=\"Account__data\">
+            <h3>Nickname</h3>
+            <p> $nickname </p>
+            <h3>Voornaam</h3>
+            <p> $firstname </p>
+            <h3>Achternaam</h3>
+            <p> $lastname </p>
+            <h3>Email</h3>
+            <p>$email </p>
+
+           </div>
 
 
 
